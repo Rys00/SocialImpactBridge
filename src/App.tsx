6fbaf6c5cc285b699236993bf6faf11ed9614247
scurek <jakub.m.sza@gmail.com>
2024-09-29 01:04:41 +0200

    10% navbara i 20% sanity left

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import SignUpForm from "./components/forms/sign-up-form.component";
+import Navbar from "./components/navbar/navbar.component.tsx";
+
 
 function App() {
   return (
-    <div>
+    <div className={'main'}>
+        <Navbar/>
       <SignUpForm />
     </div>
   );
