a3c0f9914320cac52a41051506ec3927c58314b4
grzan <84906236+SpeedfireV@users.noreply.github.com>
2024-09-29 09:11:42 +0200

    cardtile

--- src/pages/home_page/home-page.component.tsx
+++ src/pages/home_page/home-page.component.tsx
@@ -1,21 +1,23 @@
+import styles from './home-page.module.scss';
+
 const HomePage = () => {
   return (
     <div>
       <div>Search For NGOs</div>
-      <div>
+      <div className={styles.row}>
         <div>Organization Type:</div>
         <div>NGOs</div> <div>Companies</div>
         <div>Volunteers</div>
       </div>{" "}
-      <div>
+      <div className={styles.row}>
         <div>Filters:</div> <div>Tags</div> <div>Country</div>{" "}
         <div>Voivodship</div>
         <div>Commune</div>
       </div>
-      <div>
+      <div className={`${styles.listTile}  `}>
         <div>"BARTEL" - FUNDACJA IM. PROFESORA KAZIMIERZA BARTLA</div>
-        <div>KRS: 0000017687</div>
-        <div>
+        <div className={styles.row}>KRS: 0000017687</div>
+        <div className={styles.rowSpaceBetween}>
           <div>E-mail: bartel@mail.com</div>
           <div>#Schroniska#Pomoc#TAGI</div>
         </div>
