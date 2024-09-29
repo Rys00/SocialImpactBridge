140ce83758421c9e683589af96fdbdf4696a2dbd
grzan <84906236+SpeedfireV@users.noreply.github.com>
2024-09-29 10:51:46 +0200

    finalised home page

--- src/pages/home_page/home-page.component.tsx
+++ src/pages/home_page/home-page.component.tsx
@@ -1,5 +1,6 @@
 import styles from './home-page.module.scss';
 import SearchBar from "./components/searchbar.component.tsx";
+import SelectBox from "../../components/inputs/select-box.component.tsx";
 // import SearchBar from "./components/searchbar.component.tsx";
 
 const HomePage = () => {
@@ -15,20 +16,37 @@ const HomePage = () => {
 search
 </button></div>
           <div className={styles.row} style={{marginTop:"8px"}}>
-              <div>Organization Type:</div>
-              <div className={styles.selectedCard} style={{marginLeft: '18px'}}>NGOs</div>
-              <div className={styles.unselectedCard} style={{marginLeft: '12px'}}>Companies</div>
-              <div className={styles.unselectedCard} style={{marginLeft: '12px'}}>Volunteers</div>
+
+              <div style={{
+                  display:"flex",
+                  flexDirection:"column",
+                  justifyContent:"center"
+              }}>Organization Type:</div>
+              <button className={styles.selectedCard} style={{marginLeft: '18px'}}>NGOs</button>
+              <button className={styles.unselectedCard} style={{marginLeft: '12px'}}>Firmy</button>
+              <button className={styles.unselectedCard} style={{marginLeft: '12px'}}>Wolontariusze & Pracownicy</button>
           </div>
-          <div className={styles.row}>
-              <div>Filters:</div>
-              <div>Tags</div>
-              <div>Country</div>
-              {" "}
-              <div>Voivodship</div>
-              <div>Commune</div>
+          <div className={styles.row} style={{marginTop:"16px", content:"flex", justifyContent:"start"}}>
+              <div style={{content:"flex", flexDirection:"column", justifyContent: "center"}}>Filters:</div>
+              <SelectBox label={"Tagi"} options={[{ name: "123", value: "" }]} style={{
+                  borderWidth:"2px",
+                  borderColor:"var(--text)",  width:"144px"
+
+              }}/>
+              <SelectBox label={"Państwo"} options={[{ name: "123", value: "" }]} style={{
+                  borderWidth:"2px",
+                  borderColor:"var(--text)",  width:"144px"
+              }}/>
+              <SelectBox label={"Województwo"} options={[{ name: "123", value: "" }]} style={{
+                  borderWidth:"2px",
+                  borderColor:"var(--text)", width:"144px"
+              }}/>
+              <SelectBox label={"Powiat"} options={[{ name: "123", value: "" }]} style={{
+                  borderWidth:"2px",
+                  borderColor:"var(--text)", width:"144px"
+              }}/>
           </div>
-          <div className={`${styles.listTile}  `}>
+          <div className={`${styles.listTile}  `} style={{marginTop:"32px"}}>
               <div>"BARTEL" - FUNDACJA IM. PROFESORA KAZIMIERZA BARTLA</div>
               <div className={styles.row}>KRS: 0000017687</div>
               <div className={styles.rowSpaceBetween}>
